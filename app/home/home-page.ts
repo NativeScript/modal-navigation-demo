4a58f8c6fadce6c4b3ab595f1a5f4e4e779d92b8
tsonevn <nikolaytsonev93@gmail.com>
2019-01-16 15:30:45 +0200

    create frame via XML

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -1,21 +1,12 @@
 import { View, EventData } from "tns-core-modules/ui/core/view";
 import { Frame } from "tns-core-modules/ui/frame";
 import { ShownModallyData } from "tns-core-modules/ui/page";
-
+const modalPath = "modal-root/modal-root-page"
 export function onModalFrame(args: EventData) {
     const view = args.object as View;
     const page = view.page;
-    const frame = new Frame();
-    const context = "Context from home-page";
 
-    frame.on("shownModally", (args: ShownModallyData) => {
-        frame.navigate({
-            moduleName: "modal/modal-page",
-            context: args.context // context to pass from showModal
-        });
-    })
-
-    page.showModal(frame, context,
+    page.showModal(modalPath, {},
         (closeCallbackContext) => {
             console.log("[home-page] closeModal callback context:", closeCallbackContext);
         }, true);
